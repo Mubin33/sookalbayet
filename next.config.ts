17cc176407fac511234a1c757bc01b5e9130ff01
Mubin33 <mubinulislam14@gmail.com>
2026-08-06 12:37:15 +0600

    chore: update pnpm-lock.yaml to fix vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  // Force reload
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
